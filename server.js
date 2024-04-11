8c7af4896aa33e8a72ec49e59cfa67182fc403b6
Daniel Ji <daniel.ji0923@gmail.com>
2024-04-11 10:19:36 -0700

    initial

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const PeopleRouter = require('./controllers/people')
+
+
+
+// APP
+const app = express();
+
+// MIDDLEWARE
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+
+// ROUTES
+app.use('/people', PeopleRouter)
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+
+// LISTENER
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`listening on port ${PORT}` || 5000))
